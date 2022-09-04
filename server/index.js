250021650ba0bd9aa772bef0d17bb9101207c90e
ronilahav <ronilahav24@gmail.com>
2022-09-04 06:49:40 +0300

    build and route from server

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const http = require("http");
+const path = require("path");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const dotenv = require("dotenv");
@@ -9,20 +10,21 @@ const db = require("./db");
 const { socketConnection } = require("./utils/socket-io");
 const videoRouter = require("./routes/video-router");
 
-const apiPort = 8000;
+const apiPort = process.env.PORT || 8000;
 const app = express();
 const server = http.createServer(app);
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, "../client/build")));
 
 socketConnection(server);
 
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
 app.use("/api", videoRouter);
